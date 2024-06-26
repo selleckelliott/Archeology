a977ab712fbb70795898070b9936adbe85dbff32
Selleck Elliott <selleck.elliott@gmail.com>
2024-06-25 18:51:09 -0600

    first commit

---
+++ Archeology/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+Console.WriteLine("Hello, World!");
